1f026a60c5e232244ce5dddab5e0e8ccb1908fee
Alex Bazlinton <abazlinton@gmail.com>
2019-02-19 10:57:01 +0000

    add overrides

--- src/main/java/Chrome.java
+++ src/main/java/Chrome.java
@@ -1,5 +1,6 @@
 public class Chrome implements IBrowser {
 
+    @Override
     public String makeGetRequest(Server server){
         IWebServer webServer = (IWebServer) server.connect(80);
         return webServer.get("/");
--- src/main/java/Express.java
+++ src/main/java/Express.java
@@ -1,9 +1,11 @@
 public class Express implements IService, IWebServer {
 
+    @Override
     public String get(String resource){
         return "<h1>Hello World</h1>";
     }
 
+    @Override
     public String getStartStatus(){
         return "Express is listening on port 80!";
     }
